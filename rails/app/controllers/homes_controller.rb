4b05013c6535abaaa4e36a5a167fed7eb7e01fba
Mikan <83708535+piny940@users.noreply.github.com>
2023-08-23 08:51:59 +0000

    rubocop

--- rails/app/controllers/homes_controller.rb
+++ rails/app/controllers/homes_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HomesController < ApplicationController
   def show; end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   mount KomachiHeartbeat::Engine => '/ops'
-  
+
   root 'homes#show'
 end
