2b48fa063abf38f79add3fe9dd313c60496e88bf
Mikan <83708535+piny940@users.noreply.github.com>
2023-08-23 06:15:49 +0000

    市区町村テーブルに読みのカラムを追加

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,9 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_054224) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_060512) do
+  create_table "branch_city_assessables", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.bigint "city_id", null: false
+    t.bigint "branch_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["branch_id"], name: "index_branch_city_assessables_on_branch_id"
+    t.index ["city_id"], name: "index_branch_city_assessables_on_city_id"
+  end
+
+  create_table "branches", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "company_id", null: false
+    t.string "logo_url", null: false
+    t.string "post_number", default: "", null: false
+    t.bigint "city_id", null: false
+    t.string "address", null: false
+    t.string "phone", default: "", null: false
+    t.string "fax_number", default: "", null: false
+    t.string "business_hours", default: "", null: false
+    t.string "regular_holiday", default: "", null: false
+    t.string "catchphrase", null: false
+    t.string "introduction", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_branches_on_city_id"
+    t.index ["company_id"], name: "index_branches_on_company_id"
+  end
+
   create_table "cities", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false
+    t.string "yomi", null: false
     t.bigint "prefecture_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -31,5 +60,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_054224) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "branch_city_assessables", "branches"
+  add_foreign_key "branch_city_assessables", "cities"
+  add_foreign_key "branches", "cities"
+  add_foreign_key "branches", "companies"
   add_foreign_key "cities", "prefectures"
 end
