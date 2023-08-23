03aa28fb9e24e15d59965500b92e75337d061559
Mikan <83708535+piny940@users.noreply.github.com>
2023-08-23 05:33:55 +0000

    prefecturesのidが自動でincrementされないようにする

--- rails/db/migrate/20230823051804_create_prefectures.rb
+++ rails/db/migrate/20230823051804_create_prefectures.rb
@@ -1,6 +1,7 @@
 class CreatePrefectures < ActiveRecord::Migration[7.0]
   def change
-    create_table :prefectures do |t|
+    create_table :prefectures, id: false, primary_key: 'id' do |t|
+      t.integer :id, null: false
       t.string :name, null: false
 
       t.timestamps
