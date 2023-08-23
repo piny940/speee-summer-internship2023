0bb99d5b18dc569bd7e0e41e5a6d992269e2f641
Yuritani2000 <50820063+Yuritani2000@users.noreply.github.com>
2023-08-23 10:03:05 +0000

    ルーティングの追加と仮のビュー・コントローラ追加

---
+++ rails/app/controllers/assessments_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AsssessmentsController < ActionController::Base
+  
+  def new
+  end
+end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   mount KomachiHeartbeat::Engine => '/ops'
 
   root 'homes#show'
-  resources :branches, only: [:show]  # 企業ページ
+
+  # この部分はさとしょーのIssue#14企業ページとコンフリクトする。適宜解決する。
+  resources :branches, only: %i[show] do
+    resource :assessments, only: %i[new]
+  end
 end
