7ed9c150327cc7c8dcb2d990b6f78f09df952b66
Mikan <83708535+piny940@users.noreply.github.com>
2023-08-24 04:08:51 +0000

    cityをCSVから読み込めるようにする

--- rails/lib/scripts/csv_data_setters.rb
+++ rails/lib/scripts/csv_data_setters.rb
@@ -10,3 +10,21 @@ def create_prefectures_from_csv(path)
     prefecture.save!
   end
 end
+
+def create_cities_from_csv(path)
+  data = CSV.read(path)[1..]
+  data.each_with_index do |row, idx|
+    city = City.find_or_initialize_by(id: row[0])
+    city.assign_attributes(
+      prefecture_id: row[1],
+      name: row[2],
+      name_yomi: row[3],
+    )
+    city.save!
+
+    # 100行ごとに出力
+    if idx % 100 == 0
+      p "Line #{idx} OK"
+    end
+  end
+end
