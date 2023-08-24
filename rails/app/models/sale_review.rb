1a49f582f1077f248620c89549dd92cf4a304568
Mikan <83708535+piny940@users.noreply.github.com>
2023-08-24 06:27:16 +0000

    create_sale_reviews_from_csvを作成

--- rails/app/models/sale_review.rb
+++ rails/app/models/sale_review.rb
@@ -43,4 +43,80 @@ class SaleReview < ApplicationRecord
     financial: 5, # 金銭的な理由
     other: 99 # その他
   }, _prefix: true
+
+  def self.create_sale_reviews_from_csv(path)
+    data = CSV.read(path)[2..]
+    data.each do |row|
+      branch = Branch.find(row[1])
+      prefecture = Prefecture.find_by(name: row[5])
+      city = City.find_by(name: row[6])
+      gender = case row[3]
+        when '男性' then 'male'
+        when '女性' then 'female'
+        else 'other'
+      property_type = case row[8]
+        when 'マンション' then 'apartment'
+        when '戸建て' then 'house'
+        when '土地' then 'land'
+      previous_experience = case row[9]
+        when '初めて' then 'first'
+        when '2回目' then 'second'
+        when '3回以上' then 'more'
+      discounted = case row[18]
+        when 1 then true
+        when 0 then false
+      agency_type = case row[23]
+        when 1 then 'senzoku_sennin'
+        when 2 then 'sennin'
+        when 3 then 'ippan'
+        when 4 then 'other'
+      sale_reason = case row[25]
+        when 1 then 'moving'
+        when 2 then 'inheritance'
+        when 3 then 'job_change'
+        when 4 then 'divorce'
+        when 5 then 'assets_management'
+        when 6 then 'financial'
+        else 'other'
+
+      # 店舗・氏名・不動産会社の対応満足度の理由の3つが全て同じ口コミは同一の口コミとみなして更新を行う
+      sale_review = find_or_initialize_by(
+        branch_id: row[1],
+        name: row[2],
+        service_satisfaction_reason: row[29]
+      )
+      sale_review.assign_attributes(
+        publishment: 'published', # CSVから取り込んだ口コミはデフォルトで公開とする
+        name: row[2],
+        gender:,
+        age: row[4],
+        city_id: city.id,
+        address: row[7],
+        property_type:,
+        previous_experience:,
+        begin_consideration_period: row[10],
+        assessment_request_period: row[11],
+        begin_sale_period: row[12],
+        sale_period: row[13],
+        transfer_period: row[14],
+        speed_satisfaction: row[15],
+        assessed_price: row[16],
+        begin_sale_price: row[17],
+        discounted:,
+        discounted_period_from_begin_sale: row[19],
+        discount_amount: row[20],
+        final_sale_price: row[21],
+        sale_price_satisfaction: row[22],
+        agency_type:,
+        title: row[24],
+        sale_reason:,
+        concerns: row[26],
+        decision_factor: row[27],
+        service_satisfaction: row[28],
+        service_satisfaction_reason: row[29],
+        advice_for_next: row[30],
+        complaint: row[31]
+      )
+    end
+  end
 end
