0bd16a0ab9a336babad533eb9eccbf739d5e70bf
Mikan <83708535+piny940@users.noreply.github.com>
2023-08-24 07:39:15 +0000

    rubocop

--- rails/app/models/sale_review.rb
+++ rails/app/models/sale_review.rb
@@ -1,42 +1,8 @@
 # frozen_string_literal: true
+
 require 'csv'
 
-GENDERS = {
-  '男性' => 'male',
-  '女性' => 'female',
-  'その他・不明' => 'other'
-}.freeze
-PROPERTY_TYPES = {
-  'マンション' => 'apartment',
-  '戸建て' => 'house',
-  '土地' => 'land'
-}.freeze
-PREVIOUS_EXPERIENCES = {
-  '初めて' => 'first',
-  '2回目' => 'second',
-  '3回以上' => 'more'
-}.freeze
-DISCOUNTEDS = {
-  '1' => true,
-  '0' => false
-}.freeze
-AGENCY_TYPES = {
-  '1' => 'senzoku_sennin',
-  '2' => 'sennin',
-  '3' => 'ippan',
-  '4' => 'other'
-}.freeze
-SALE_REASONS = {
-  '1' => 'moving',
-  '2' => 'inheritance',
-  '3' => 'job_change',
-  '4' => 'divorce',
-  '5' => 'assets_management',
-  '6' => 'financial',
-  '99' => 'other'
-}.freeze
-
-class SaleReview < ApplicationRecord
+class SaleReview < ApplicationRecord # rubocop:disable Metrics/ClassLength
   belongs_to :branch
   belongs_to :city
 
@@ -83,9 +49,7 @@ class SaleReview < ApplicationRecord
   def self.create_sale_reviews_from_csv(path)
     data = CSV.read(path)[2..]
     data.each_with_index do |row, idx|
-      Branch.find(row[1])
-      Prefecture.find_by(name: row[5])
-      city = City.find_by(name: row[6])
+      review_data = csv_row_to_hash(row)
 
       # 店舗・氏名・不動産会社の対応満足度の理由の3つが全て同じ口コミは同一の口コミとみなして更新を行う
       sale_review = find_or_initialize_by(
@@ -93,38 +57,7 @@ class SaleReview < ApplicationRecord
         name: row[2],
         service_satisfaction_reason: row[29]
       )
-      sale_review.assign_attributes(
-        publishment: 'published', # CSVから取り込んだ口コミはデフォルトで公開とする
-        name: row[2],
-        gender: GENDERS[row[3]],
-        age: row[4],
-        city_id: city.id,
-        address: row[7],
-        property_type: PROPERTY_TYPES[row[8]],
-        previous_experience: PREVIOUS_EXPERIENCES[row[9]],
-        begin_consideration_period: row[10],
-        assessment_request_period: row[11],
-        begin_sale_period: row[12],
-        sale_period: row[13],
-        transfer_period: row[14],
-        speed_satisfaction: row[15],
-        assessed_price: row[16],
-        begin_sale_price: row[17],
-        discounted: DISCOUNTEDS[row[18]],
-        discounted_period_from_begin_sale: row[19] || -1,
-        discount_amount: row[20] || 0,
-        final_sale_price: row[21],
-        sale_price_satisfaction: row[22],
-        agency_type: AGENCY_TYPES[row[23]],
-        title: row[24],
-        sale_reason: SALE_REASONS[row[25]],
-        concerns: row[26],
-        decision_factor: row[27],
-        service_satisfaction: row[28],
-        service_satisfaction_reason: row[29],
-        advice_for_next: row[30],
-        complaint: row[31] || 0
-      )
+      sale_review.assign_attributes(review_data)
       sale_review.save!
 
       # RawSaleReviewの作成も同時に行う
@@ -133,41 +66,85 @@ class SaleReview < ApplicationRecord
         name: row[2],
         service_satisfaction_reason: row[29]
       )
-      raw_sale_review.assign_attributes(
-        name: row[2],
-        gender: GENDERS[row[3]],
-        age: row[4],
-        city_id: city.id,
-        address: row[7],
-        property_type: PROPERTY_TYPES[row[8]],
-        previous_experience: PREVIOUS_EXPERIENCES[row[9]],
-        begin_consideration_period: row[10],
-        assessment_request_period: row[11],
-        begin_sale_period: row[12],
-        sale_period: row[13],
-        transfer_period: row[14],
-        speed_satisfaction: row[15],
-        assessed_price: row[16],
-        begin_sale_price: row[17],
-        discounted: DISCOUNTEDS[row[18]],
-        discounted_period_from_begin_sale: row[19] || -1,
-        discount_amount: row[20] || 0,
-        final_sale_price: row[21],
-        sale_price_satisfaction: row[22],
-        agency_type: AGENCY_TYPES[row[23]],
-        title: row[24],
-        sale_reason: SALE_REASONS[row[25]],
-        concerns: row[26],
-        decision_factor: row[27],
-        service_satisfaction: row[28],
-        service_satisfaction_reason: row[29],
-        advice_for_next: row[30],
-        complaint: row[31] || 0
-      )
+      review_data.delete(:publishment) # 公開/非公開はRawSaleReviewでは考えない
+      raw_sale_review.assign_attributes(review_data)
       raw_sale_review.save!
 
       # 100行ごとに出力
       Logger.new($stdout).debug "Line #{idx} OK" if (idx % 100).zero?
     end
+  end
+
+  GENDERS = {
+    '男性' => 'male',
+    '女性' => 'female',
+    'その他・不明' => 'other'
+  }.freeze
+  PROPERTY_TYPES = {
+    'マンション' => 'apartment',
+    '戸建て' => 'house',
+    '土地' => 'land'
+  }.freeze
+  PREVIOUS_EXPERIENCES = {
+    '初めて' => 'first',
+    '2回目' => 'second',
+    '3回以上' => 'more'
+  }.freeze
+  DISCOUNTEDS = {
+    '1' => true,
+    '0' => false
+  }.freeze
+  AGENCY_TYPES = {
+    '1' => 'senzoku_sennin',
+    '2' => 'sennin',
+    '3' => 'ippan',
+    '4' => 'other'
+  }.freeze
+  SALE_REASONS = {
+    '1' => 'moving',
+    '2' => 'inheritance',
+    '3' => 'job_change',
+    '4' => 'divorce',
+    '5' => 'assets_management',
+    '6' => 'financial',
+    '99' => 'other'
+  }.freeze
+  def self.csv_row_to_hash(row)
+    Branch.find(row[1])
+    Prefecture.find_by(name: row[5])
+    city = City.find_by(name: row[6])
+
+    {
+      publishment: 'published', # CSVから入ってきたデータはデフォルトで公開する
+      name: row[2],
+      gender: GENDERS[row[3]],
+      age: row[4],
+      city_id: city.id,
+      address: row[7],
+      property_type: PROPERTY_TYPES[row[8]],
+      previous_experience: PREVIOUS_EXPERIENCES[row[9]],
+      begin_consideration_period: row[10],
+      assessment_request_period: row[11],
+      begin_sale_period: row[12],
+      sale_period: row[13],
+      transfer_period: row[14],
+      speed_satisfaction: row[15],
+      assessed_price: row[16],
+      begin_sale_price: row[17],
+      discounted: DISCOUNTEDS[row[18]],
+      discounted_period_from_begin_sale: row[19] || -1,
+      discount_amount: row[20] || 0,
+      final_sale_price: row[21],
+      sale_price_satisfaction: row[22],
+      agency_type: AGENCY_TYPES[row[23]],
+      title: row[24],
+      sale_reason: SALE_REASONS[row[25]],
+      concerns: row[26],
+      decision_factor: row[27],
+      service_satisfaction: row[28],
+      service_satisfaction_reason: row[29],
+      advice_for_next: row[30],
+      complaint: row[31] || ''
+    }
   end
 end
