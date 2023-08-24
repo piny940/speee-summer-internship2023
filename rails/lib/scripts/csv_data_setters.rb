d951d83221cc1967b70809272c02a28c4762b578
Mikan <83708535+piny940@users.noreply.github.com>
2023-08-24 02:55:22 +0000

    Prefectureを登録する機能を作成

--- rails/lib/scripts/csv_data_setters.rb
+++ rails/lib/scripts/csv_data_setters.rb
@@ -1,4 +1,7 @@
 def set_prefectures_from_csv(path)
-  data = CSV.read(path)
-  p data
+  data = CSV.read(path)[1..]
+  data.each do |row|
+    pref = Prefecture.find_or_initialize_by(id: row['id'])
+    pref.update_attributes!(name: row['name'])
+  end
 end
--- rails/lib/scripts/read_set_data_from_csv.rb
+++ rails/lib/scripts/read_set_data_from_csv.rb
@@ -1,3 +1,4 @@
-require './csv_data_setters'
+require './lib/scripts/csv_data_setters'
+require 'csv'
 
 set_prefectures_from_csv('lib/assets/prefectures.csv')
