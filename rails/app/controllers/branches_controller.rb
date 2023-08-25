28b3139a86684bd77c986e1c8c9d05c84dbe5c6c
sts11142 <73372024+sts11142@users.noreply.github.com>
2023-08-25 08:46:35 +0000

    [update] ActiveRecordの関連を使用 + コードのゴミを削除

--- rails/app/controllers/branches_controller.rb
+++ rails/app/controllers/branches_controller.rb
@@ -1,8 +1,7 @@
 class BranchesController < ApplicationController
   def show
     @branch = Branch.find(params[:id])
-    @sale_reviews = SaleReview.where(branch_id: @branch.id).page(params[:page])
-    @debug_placeholder = "（変数未登録）"
+    @sale_reviews = @branch.sale_reviews.page(params[:page])
   end
 end
 
