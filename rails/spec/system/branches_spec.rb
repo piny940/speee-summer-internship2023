313cdb9608c6f951774f9da1c7e46e78f18313d0
sts11142 <73372024+sts11142@users.noreply.github.com>
2023-08-25 06:41:29 +0000

    [add] factory botの使い方を確認→テストのサンプルを追加

--- rails/spec/system/branches_spec.rb
+++ rails/spec/system/branches_spec.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # frozen_string_literal: true
 
 require 'rails_helper'
@@ -55,30 +54,4 @@ RSpec.describe 'Branches' do
     end
   end
 end
-=======
-require 'rails_helper'
-
-RSpec.describe "Users", type: :system do
-  # before do
-  #   driven_by(:rack_test)
-  # end
-  
-  describe '必須要素の確認' do
-    context 'データが正しく加工されていることを確認' do  
-      it '平均評価が' do
-        
-      end
-    end
-
-    context '企業情報が正しく表示されていることを確認' do
-    end
-  end
-
-  describe 'ページ遷移確認' do
-    context '査定ページへ正しく遷移する場合' do
-    end
-
-    context '査定ページへ正しく遷移しない場合'
-  end
-end
->>>>>>> 27c9a4a ([add] （基盤）テストが動くことを確認)
+ß
