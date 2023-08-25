7a83e2d05ed1063e14b30ca01f6f38bc45ba2637
sts11142 <73372024+sts11142@users.noreply.github.com>
2023-08-25 08:53:43 +0000

    [delete] このissueではテストをしないので、テスト関連で中途半端に追加したファイルとコードを削除
    `branch-page-test`ブランチでは今回削除したファイルをいくつか使用しているので、将来この部分でコンフリクトが起きたら頑張って対処する

--- rails/spec/factories/companies.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :company do
-    sequence(:name) { |n| "ベテすま不動産#{n}" }
-    created_at { '2020-11-07 09:28:28 +0900' }
-    updated_at { '2021-11-07 09:28:28 +0900' }
-    end
-end  
