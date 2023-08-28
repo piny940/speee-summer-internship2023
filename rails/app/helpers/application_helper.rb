86ca8217829646c459718886a73a21da42286f41
piny940 <an.s.shoma@gmail.com>
2023-08-28 13:25:13 +0900

    format_priceを使用するようにする

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  ## xx万円という形の文字列にする
   def format_price(price)
     "#{(price / 10_000).to_fs(:delimited)}万円"
+  end
+
+  ## Y年m月という形にする
+  def year_month_description(datetime)
+    datetime.strftime("%Y年#{datetime.month}月")
   end
 end
--- rails/app/helpers/datetime_helper.rb
+++
@@ -1,5 +0,0 @@
-module DatetimeHelper
-  def year_month_description(datetime)
-    datetime.strftime("%Y年#{datetime.month}月")
-  end
-end
