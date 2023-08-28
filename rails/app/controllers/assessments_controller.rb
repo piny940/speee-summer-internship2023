14984773111a507be5508216d8635d48cda4ad44
Yuritani2000 <50820063+Yuritani2000@users.noreply.github.com>
2023-08-28 00:45:25 +0000

    stash回復時のコンフリクト対処

--- rails/app/controllers/assessments_controller.rb
+++ rails/app/controllers/assessments_controller.rb
@@ -4,6 +4,8 @@ class AssessmentsController < ApplicationController
   def new
     @assessment_user = AssessmentUser.new
     @assessment = Assessment.new
+    @prefecture = Prefecture.new
+    @cities = City.all
   end
 
   def create
@@ -36,15 +38,18 @@ class AssessmentsController < ApplicationController
                                                          :property_constructed_year)
   end
 
+  def prefecture_params
+    params[:assessment_user].require(:prefecture).permit(:id)
+  end
+
   def save_assessment_user_and_assessment
     if @assessment_user.valid? && @assessment.valid?
       ActiveRecord::Base.transaction do
         @assessment_user.save!
         @assessment.save!
       end
-      redirect_to assessments_success_path
-    else
-      render 'new', status: :bad_request
+    elsif params[:query_prefecture]
+      render 'new', status: :multiple_choices
     end
   end
 end
