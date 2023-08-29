ada7ceefcb3c53f856862d0fd571c21259761786
piny940 <an.s.shoma@gmail.com>
2023-08-29 15:24:42 +0900

    change_columnに対応するコードをdown内にも記述

--- rails/db/migrate/20230829053329_change_columns_of_assessment_for_miniul_api.rb
+++ rails/db/migrate/20230829053329_change_columns_of_assessment_for_miniul_api.rb
@@ -12,5 +12,8 @@ class ChangeColumnsOfAssessmentForMiniulApi < ActiveRecord::Migration[7.0]
   def down
     add_column :assessments, :property_building_area_unit, :integer, null: false, default: 0, comment: '建物面積の単位'
     add_column :assessments, :property_floor_area, :float, null: false, default: 0, comment: '対象物件の延床面積'
+    change_column :assessments, :property_exclusive_area, :float, null: false, comment: '対象物件の専有面積'
+    change_column :assessments, :property_land_area, :float, null: false, comment: '対象物件の土地面積'
+    change_column :assessments, :property_building_area, :float, null: false, comment: '対象物件の建物面積'
   end
 end
