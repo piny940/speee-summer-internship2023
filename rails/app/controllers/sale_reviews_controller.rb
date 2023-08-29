c1237ec477557c03aaf70fc94ba8351d7c740f6d
piny940 <an.s.shoma@gmail.com>
2023-08-29 11:46:30 +0900

    tmp: パンクズリストを作成

--- rails/app/controllers/sale_reviews_controller.rb
+++ rails/app/controllers/sale_reviews_controller.rb
@@ -2,6 +2,7 @@
 
 class SaleReviewsController < ApplicationController
   before_action :set_sale_review, only: %i[show]
+  before_action :set_branch, only: %i[show]
 
   def show; end
 
@@ -9,5 +10,9 @@ class SaleReviewsController < ApplicationController
 
   def set_sale_review
     @sale_review = SaleReview.find(params[:id])
+  end
+
+  def set_branch
+    @branch = @sale_review.branch
   end
 end
--- rails/app/helpers/breadcrumbs_helper.rb
+++ rails/app/helpers/breadcrumbs_helper.rb
@@ -11,7 +11,8 @@ module BreadcrumbsHelper
 
   def breadcrumbs_tree
     [
-      ['homes#show']
+      ['homes#show', 'branches#show', 'sale_reviews#show'],
+      ['homes#show', 'branches#show', 'assessments#new', 'assessments#success'],
     ]
   end
 
@@ -19,6 +20,14 @@ module BreadcrumbsHelper
     case name
     when 'homes#show'
       { name: 'トップページ', url: root_path }
+    when 'branches#show'
+      { name: "#{@branch.company.name}#{@branch.name}", url: branch_path(@branch) }
+    when 'sale_reviews#show'
+      { name: "#{@sale_review.title[..10]}...", url: sale_review_path(@sale_review)}
+    when 'assessments#new'
+      { name: '査定依頼フォーム', url: new_branch_assessment_path(@branch)}
+    when 'assessments#success'
+      { name: '査定依頼完了', url: branch_assessments_success_path(@branch)}
     else
       raise "breadcrumbs_url に #{name} が定義されていません！"
     end
