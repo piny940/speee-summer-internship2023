8c7dae29b71623ae7a238b68b00d19afe18431d5
piny940 <an.s.shoma@gmail.com>
2023-08-29 16:32:25 +0900

    rubocop

--- rails/app/controllers/assessments_controller.rb
+++ rails/app/controllers/assessments_controller.rb
@@ -16,13 +16,13 @@ class AssessmentsController < ApplicationController
 
     return render 'new', status: :bad_request \
       unless save_assessment_user_and_assessment(@assessment_user, @assessment)
-    
+
     response = create_miniul_assessment(@assessment_user, @assessment)
 
-    if response.code == '200'
+    if response.code == '201'
       redirect_to success_branch_assessments_path(@branch)
     else
-      redirect_to new_branch_assessment_path(@branch), alert: '査定依頼に失敗しました。'
+      redirect_to new_branch_assessment_path(@branch), alert: I18n.t('messages.assessment.miniul_api_fail')
     end
   end
 
@@ -53,6 +53,7 @@ class AssessmentsController < ApplicationController
 
   def save_assessment_user_and_assessment(user, assessment)
     return false unless user.valid? && assessment.valid?
+
     ActiveRecord::Base.transaction do
       @assessment_user.save!
       @assessment.save!
@@ -61,19 +62,19 @@ class AssessmentsController < ApplicationController
 
   def create_miniul_assessment(assessment_user, assessment)
     Miniul.create_assessment({
-      branch_id: assessment.branch.id,
-      property_city: assessment.city.id,
-      property_address: assessment.property_address,
-      property_type: Assessment.property_types[assessment.property_type],
-      property_exclusive_area: assessment.property_exclusive_area,
-      property_land_area: assessment.property_land_area,
-      property_building_area: assessment.property_building_area,
-      property_room_plan: Assessment.property_room_plans[assessment.property_room_plan],
-      property_constructed_year: assessment.property_constructed_year,
-      user_email: assessment_user.email,
-      user_name: "#{assessment_user.last_name} #{assessment_user.first_name}",
-      user_name_kana: "#{assessment_user.last_name_kana} #{assessment_user.first_name_kana}",
-      user_tel: assessment_user.tel
-    })
+                               branch_id: assessment.branch.id,
+                               property_city: assessment.city.id,
+                               property_address: assessment.property_address,
+                               property_type: Assessment.property_types[assessment.property_type],
+                               property_exclusive_area: assessment.property_exclusive_area,
+                               property_land_area: assessment.property_land_area,
+                               property_building_area: assessment.property_building_area,
+                               property_room_plan: Assessment.property_room_plans[assessment.property_room_plan],
+                               property_constructed_year: assessment.property_constructed_year,
+                               user_email: assessment_user.email,
+                               user_name: "#{assessment_user.last_name} #{assessment_user.first_name}",
+                               user_name_kana: "#{assessment_user.last_name_kana} #{assessment_user.first_name_kana}",
+                               user_tel: assessment_user.tel
+                             })
   end
 end
--- rails/app/models/concerns/miniul.rb
+++ rails/app/models/concerns/miniul.rb
@@ -1,16 +1,14 @@
+# frozen_string_literal: true
+
 require 'net/http'
 
 module Miniul
-  CONVERSIONS_ENDPOINT = "https://miniul-api.herokuapp.com/affiliate/v2/conversions"
+  CONVERSIONS_ENDPOINT = 'https://miniul-api.herokuapp.com/affiliate/v2/conversions'
   def self.create_assessment(params)
     uri = URI.parse(CONVERSIONS_ENDPOINT)
     http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = uri.scheme = "https"
-    headers = { "Content-Type" => "application/json" }
-    data = {
-      **params,
-      url_params: 'beteran-sumai'
-    }
+    http.use_ssl = uri.scheme = 'https'
+    headers = { 'Content-Type' => 'application/json' }
     http.post(uri.path, params.to_json, headers)
   end
 end
